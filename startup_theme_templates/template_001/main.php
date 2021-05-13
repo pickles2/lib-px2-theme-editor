6b0763efd7488ba2fe820d3a1182d8de3f5c522c
Tomoya Koyanagi <tomk79@gmail.com>
2021-05-14 02:35:48 +0900

    <Startup> テンプレート処理用に `TwigHelper` を追加

--- startup_theme_templates/template_001/main.php
+++ startup_theme_templates/template_001/main.php
@@ -16,6 +16,23 @@ class main{
 	 * テンプレートにオプション値を反映させる
 	 */
 	public function bind( $realpath_theme_root, $options ){
+
+		$templateFileList = array(
+			'theme_files/modules.css',
+		);
+
+		foreach ($templateFileList as $templateFile) {
+			$templateSrc = $this->main->fs()->read_file($realpath_theme_root.$templateFile);
+
+			$twigHelper = new \pickles2\libs\themeEditor\TwigHelper();
+			$templateSrc = $twigHelper->bind( $templateSrc, $options );
+
+			$this->main->fs()->save_file(
+				$realpath_theme_root.$templateFile,
+				$templateSrc
+			);
+		}
+
 		return true;
 	}
 
--- startup_theme_templates/template_002/main.php
+++ startup_theme_templates/template_002/main.php
@@ -12,11 +12,28 @@ class main{
 		$this->main = $main;
 	}
 
-    /**
-     * テンプレートにオプション値を反映させる
-     */
-    public function bind( $realpath_theme_root, $options ){
-        return true;
-    }
+	/**
+	 * テンプレートにオプション値を反映させる
+	 */
+	public function bind( $realpath_theme_root, $options ){
+
+		$templateFileList = array(
+			'theme_files/modules.css',
+		);
+
+		foreach ($templateFileList as $templateFile) {
+			$templateSrc = $this->main->fs()->read_file($realpath_theme_root.$templateFile);
+
+			$twigHelper = new \pickles2\libs\themeEditor\TwigHelper();
+			$templateSrc = $twigHelper->bind( $templateSrc, $options );
+
+			$this->main->fs()->save_file(
+				$realpath_theme_root.$templateFile,
+				$templateSrc
+			);
+		}
+
+		return true;
+	}
 
 }
