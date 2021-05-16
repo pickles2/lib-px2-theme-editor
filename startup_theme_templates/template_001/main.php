c8fe4d491904dc7aea09513f60a903ec2789339c
Tomoya Koyanagi <tomk79@gmail.com>
2021-05-16 11:07:49 +0900

    <Startup> 選択したロゴを Broccoli で再選択できるように修正

--- startup_theme_templates/template_001/main.php
+++ startup_theme_templates/template_001/main.php
@@ -19,6 +19,9 @@ class main{
 
 		$templateFileList = array(
 			'theme_files/modules.css',
+			'guieditor.ignore/default/data/data.json',
+			'guieditor.ignore/popup/data/data.json',
+			'guieditor.ignore/top/data/data.json',
 			'broccoli_module_packages/_env.scss',
 			'default.html',
 			'popup.html',
@@ -35,6 +38,39 @@ class main{
 				$realpath_theme_root.$templateFile,
 				$templateSrc
 			);
+		}
+
+
+		if( array_key_exists('logoImage', $options) && strlen( $options['logoImage'] ) ){
+			foreach( array('default', 'popup', 'top') as $layoutName ){
+
+				$this->main->fs()->mkdir_r( $realpath_theme_root.'theme_files/layouts/'.$layoutName.'/resources/' );
+				$this->main->fs()->copy_r(
+					$realpath_theme_root.'theme_files/logo.'.$options['logoImageExt'],
+					$realpath_theme_root.'theme_files/layouts/'.$layoutName.'/resources/logo_'.$layoutName.'.'.$options['logoImageExt']
+				);
+
+				$this->main->fs()->mkdir_r( $realpath_theme_root.'guieditor.ignore/'.$layoutName.'/data/resources/logoImage_'.$layoutName.'/' );
+				$this->main->fs()->copy_r(
+					$realpath_theme_root.'theme_files/logo.'.$options['logoImageExt'],
+					$realpath_theme_root.'guieditor.ignore/'.$layoutName.'/data/resources/logoImage_'.$layoutName.'/bin.'.$options['logoImageExt']
+				);
+
+				$resJson = array();
+				$resJson["ext"] = $options['logoImageExt'];
+				$resJson["size"] = filesize($realpath_theme_root.'theme_files/logo.'.$options['logoImageExt']);
+				$resJson["base64"] = base64_encode( file_get_contents($realpath_theme_root.'theme_files/logo.'.$options['logoImageExt']) );
+				$resJson["md5"] = md5_file($realpath_theme_root.'theme_files/logo.'.$options['logoImageExt']);
+				$resJson["isPrivateMaterial"] = false;
+				$resJson["publicFilename"] = "logo_".$layoutName;
+				$resJson["field"] = "image";
+				$resJson["fieldNote"] = [];
+				$resJson["type"] = $options['logoImageMimeType'];
+				$this->main->fs()->save_file(
+					$realpath_theme_root.'guieditor.ignore/'.$layoutName.'_/data/resources/logoImage_'.$layoutName.'/res.json',
+					json_encode($resJson)
+				);
+			}
 		}
 
 		return true;
