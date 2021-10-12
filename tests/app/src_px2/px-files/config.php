cdeeac4ab622832ddb41ef34d27953bab0026a12
Tomoya Koyanagi <tomk79@gmail.com>
2021-10-12 23:13:59 +0900

    update `template_001b`: 新モジュール `backtopagetop` を追加

--- tests/app/src_px2/px-files/config.php
+++ tests/app/src_px2/px-files/config.php
@@ -91,17 +91,14 @@ return call_user_func( function(){
 		// PX=clearcache
 		'picklesFramework2\commands\clearcache::register' ,
 
-		 // PX=config
+		// PX=config
 		'picklesFramework2\commands\config::register' ,
 
-		 // PX=phpinfo
+		// PX=phpinfo
 		'picklesFramework2\commands\phpinfo::register' ,
 
 		// sitemapExcel
 		'tomk79\pickles2\sitemap_excel\pickles_sitemap_excel::exec' ,
-
-		// PX=px2dthelper
-		'tomk79\pickles2\px2dthelper\main::register'
 	];
 
 	// funcs: Before content
@@ -113,6 +110,8 @@ return call_user_func( function(){
 		// PX=publish
 		'picklesFramework2\commands\publish::register' ,
 
+		// PX=px2dthelper
+		'tomk79\pickles2\px2dthelper\main::register' ,
 	];
 
 
