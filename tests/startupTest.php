a5a169d8d1baffbe1033411abfdb065d34b51db1
Tomoya Koyanagi <tomk79@gmail.com>
2023-01-05 00:59:50 +0900

    テンプレートから新規テーマを生成するテストを追加 (3)

--- tests/startupTest.php
+++ tests/startupTest.php
@@ -3,10 +3,13 @@
 class startupTest extends PHPUnit\Framework\TestCase{
 
 	private $fs;
+	private $px2query;
 
 	public function setUp() : void{
 		mb_internal_encoding('UTF-8');
 		$this->fs = new tomk79\filesystem();
+		require_once(__DIR__.'/testHelper/pickles2query.php');
+		$this->px2query = new testHelper_pickles2query();
 	}
 
 	/**
@@ -43,16 +46,45 @@ class startupTest extends PHPUnit\Framework\TestCase{
 				$options = json_decode(json_encode($optionsBase), true);
 				$options["templateId"] = $template_id;
 
+				// --------------------------------------
+				// 空白のテーマを新設する
 				$result = $main->gpi(array(
 					'api'=>'addNewTheme',
 					'newThemeId' => $template_id.'--'.$option_id,
 					'importFrom' => null,
 				));
+
+				// --------------------------------------
+				// テンプレートから生成する
 				$result = $main->gpi(array(
 					'api'=>'startupTheme',
 					'themeId' => $template_id.'--'.$option_id,
 					'options' => $options,
 				));
+
+				// --------------------------------------
+				// Broccoli コンテンツをリビルドする
+				// TODO: ↓各レイアウトごとにビルドするコマンドに修正する
+				$layout_list = $this->fs->ls(__DIR__.'/../startup_theme_templates/'.urlencode($template_id).'/basefiles/guieditor.ignore');
+				if( !$layout_list ){
+					$layout_list = array();
+				}
+				foreach($layout_list as $layout_id){
+					$options = array(
+						'api' => 'broccoliBridge',
+						'forBroccoli' => array(
+							'api' => 'updateContents',
+							'options' => array(
+								'lang' => 'ja',
+							),
+						),
+					);
+					$output = $this->px2query->query( [
+						__DIR__.'/app/src_px2/.px_execute.php',
+						'/'.urlencode($template_id.'--'.$option_id).'/'.urlencode($layout_id).'.html?PX=px2dthelper.px2ce.gpi&appMode=web&target_mode=theme_layout&data='.base64_encode(json_encode($options))
+					] );
+					// var_dump($output);
+				}
 			}
 		}
 
