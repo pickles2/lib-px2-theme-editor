a6345ee010afc4247d262f567da6824a6afde95a
Tomoya Koyanagi <tomk79@gmail.com>
2023-02-26 17:49:29 +0900

    ハンバーガーメニューのスタイル改善

--- startup_theme_templates/template_001b/src/modules/layouts/hamburger-menu/module.js
+++ startup_theme_templates/template_001b/src/modules/layouts/hamburger-menu/module.js
@@ -1,51 +1,64 @@
 (function(){
 	const $ = require('jquery');
-	const $body = $('body');
 	const isIphone = window.navigator.userAgent.match(/iPhone/);
-	let presetOverflow = {};
-	$(document)
-		.off('click', '.theme-hamburger-menu__open-btn button')
-		.on('click', '.theme-hamburger-menu__open-btn button', function(){
-			const $menu = $(this).closest('.theme-hamburger-menu').find('.theme-hamburger-menu__body');
-			$menu
-				.addClass('theme-hamburger-menu__body--visible')
-				.removeClass('theme-hamburger-menu__body--closed')
-				;
 
-			presetOverflow = {
-				"hasClass": $body.hasClass('theme-scroll-lock'),
-				"scrollY": window.scrollY,
-				"scrollX": window.scrollX,
-				"css_overflow": $body.css('overflow'),
-				"css_top": $body.css('top'),
-			};
-			$body.addClass('theme-scroll-lock');
-			if( isIphone ){
+	$(function(){
+		const $body = $('body');
+		const $hamburgerMenus = $('.theme-hamburger-menu');
+		let presetOverflow = {};
+
+		function bodyScrollLockSwitch( flgOn ){
+			if( flgOn ) {
+				presetOverflow = {
+					"hasClass": $body.hasClass('theme-scroll-lock'),
+					"scrollY": window.scrollY,
+					"scrollX": window.scrollX,
+					"css_overflow": $body.css('overflow'),
+					"css_top": $body.css('top'),
+				};
+				$body.addClass('theme-scroll-lock');
+				if( isIphone ){
+					$body
+						.addClass('theme-scroll-lock--iphone')
+						.css({"top": presetOverflow.scrollY * -1});
+				}
+			}else{
 				$body
-					.addClass('theme-scroll-lock--iphone')
-					.css({"top": presetOverflow.scrollY * -1});
+					.removeClass('theme-scroll-lock')
+					.removeClass('theme-scroll-lock--iphone');
+				if( isIphone ){
+					window.scroll(presetOverflow.scrollX, presetOverflow.scrollY);
+					$body.css({"top": presetOverflow.css_top});
+				}
 			}
+		}
 
-		})
-		.off('click', '.theme-hamburger-menu__close-btn button')
-		.on('click', '.theme-hamburger-menu__close-btn button', function(){
-			const $menu = $(this).closest('.theme-hamburger-menu').find('.theme-hamburger-menu__body');
-			$menu
-				.removeClass('theme-hamburger-menu__body--visible')
-				.addClass('theme-hamburger-menu__body--closed')
-				;
-			setTimeout(function(){
-				$menu
-					.removeClass('theme-hamburger-menu__body--closed')
-					;
-			}, 300);
+		$hamburgerMenus.each(function(hamburgerMenuIndex, elmHamburgerMenu){
+			const $hamburgerMenu = $(elmHamburgerMenu);
+			const $toggleButton = $hamburgerMenu.find('button.theme-hamburger-menu__open-btn');
 
-			$body
-				.removeClass('theme-scroll-lock')
-				.removeClass('theme-scroll-lock--iphone');
-			if( isIphone ){
-				window.scroll(presetOverflow.scrollX, presetOverflow.scrollY);
-				$body.css({"top": presetOverflow.css_top});
-			}
+			$toggleButton
+				.off('click.theme-hamburger-menu')
+				.on('click.theme-hamburger-menu', function(){
+					if( !$hamburgerMenu.hasClass('theme-hamburger-menu--visible') ){
+						// 開く
+						$hamburgerMenu.addClass('theme-hamburger-menu--visible');
+						setTimeout(function(){
+							$hamburgerMenu.addClass('theme-hamburger-menu--opening');
+						}, 0);
+						bodyScrollLockSwitch(true);
+						return;
+					}else{
+						// 閉じる
+						$hamburgerMenu.removeClass('theme-hamburger-menu--opening');
+						setTimeout(function(){
+							$hamburgerMenu.removeClass('theme-hamburger-menu--visible');
+						}, 300);
+
+						bodyScrollLockSwitch(false);
+						return;
+					}
+				});
 		});
+	});
 })();
