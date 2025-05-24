1e429dc3494dc1474c235d6af0dcf680a3ce0584
Tomoya Koyanagi <tomk79@gmail.com>
2025-05-24 19:31:26 +0900

    テーマテンプレート `Kaleflower` を追加した。 (11)

--- tests/app/src_px2/px-files/config.php
+++ tests/app/src_px2/px-files/config.php
@@ -157,7 +157,7 @@ return call_user_func( function(){
 			'cookie_theme_switch'=>'THEME',
 			'path_theme_collection'=>'./px-files/themes/',
 			'attr_bowl_name_by'=>'data-contents-area',
-			'default_theme_id' => 'pickles2_2023--full'
+			'default_theme_id' => 'kaleflower--full'
 		]).')' ,
 
 		// Apache互換のSSIの記述を解決する
